f78ed224bbea3c8f8582ab31c12df806906ab4ae
wayscon <125749887+wayscon@users.noreply.github.com>
2023-07-18 13:21:15 +0500

    Add files via upload

---
+++ html/app.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', e =>{
+//     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+// })
+// WebGLSampler.registerPlugin(ScrollTrigger, ScrollSmoother)
+// ScrollSmoother.create ({
+//     wrapper: '.wrapper',
+//     content: '.content'
+// })
